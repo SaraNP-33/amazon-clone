0efa23207998fcad093ecd4ef665e14768e892e3
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2023-06-16 18:46:15 +0000

    added the components Checkout product, advert and subtotal that are now rendering in checkout page. Added logic to render the products, the subtotal, the quantity and the ability to remove items from cart

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,14 +1,52 @@
 import Head from 'next/head'
 import {
-  Container,
+  Container, Main,
   
 } from '../styles/sharedstyles'
+import Advert from '../components/Advert'
+import { CheckoutBody, CheckoutContainer, CheckoutTitle } from '../styles/chechoutstyles'
+import { useAppSelector } from '../lib/store/hooks'
+import { selectCartItems, selectCartItemsCount } from '../lib/store/reducers/cartReducer'
+import { CartType } from '../types/cart'
+import CheckoutProduct from '../components/CheckoutProduct'
+import SubTotal from '../components/SubTotal'
 
 
 export default function Checkout() {
+
+  const cart= useAppSelector(selectCartItems);
+  const cartCount =useAppSelector(selectCartItemsCount);
   return (
     <Container>
-      <p>checkout</p>
+      <Main>
+      <Advert/>
+      <CheckoutBody>
+        <CheckoutContainer>
+          {cart.length === 0? (
+            <div>
+              <h2>Your Shopping Cart is Empty</h2>
+            </div>
+          ):(
+            <div>
+              <CheckoutTitle>
+                  Your Shopping Cart - {cartCount}{" "}
+                  {cartCount > 1 ? "items" : "item"} in Cart
+                </CheckoutTitle>
+                {cart.map((item: CartType, index: number) => (
+                  <CheckoutProduct
+                    key={index}
+                    id={item.id}
+                    title={item.title}
+                    image={item.image}
+                    price={item.price}
+                  />
+                ))}
+            </div>
+          )}
+        </CheckoutContainer>
+        {cart?.length > 0 && <SubTotal />}
+      </CheckoutBody>
+      </Main>
     </Container>
   )
 }
